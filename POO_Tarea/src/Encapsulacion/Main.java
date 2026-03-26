342600f4dfcd40939ffda73605e01b7b7f31c304
Norbey <norbeycastillo890@gmail.com>
2026-03-26 17:33:17 -0600

    ejercicio 1

---
+++ POO_Tarea/src/Encapsulacion/Main.java
@@ -0,0 +1,25 @@
+package Encapsulacion;
+
+
+public class Main {
+	
+	public static void ejercicio1() {
+		
+		Persona p1 = new Persona("Norbey", "Castillo", 20);
+		Persona p2 = new Persona("Marlyn", "Vanesa", 27);
+		Persona p3 = new Persona("Mivian", "Elizabeth", 34);
+		
+		p1.mostrar();
+		p2.mostrar();
+		p3.mostrar();
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+		ejercicio1();
+
+	}
+
+}
